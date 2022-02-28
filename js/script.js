d1bb400b125ece275fdd3aded6bd3206ec5f3522
ImPaulinTech <impaulintech@gmail.com>
2022-02-28 22:17:40 +0800

    Update Navbar

--- js/script.js
+++ js/script.js
@@ -4,18 +4,27 @@ const menu_btn = document.querySelector(".menu");
 const close = document.querySelector(".close");
 const overlay = document.querySelector(".nav_overlay");
 
+//Remove preload class in body tag when windows loaded.
 window.addEventListener("load", () => {
   document.body.classList.remove("preload");
 });
+
+//Open navbar when hamburger menu was clicked
 menu.addEventListener("click", () => {
   nav.classList.add("nav-open");
 });
+
+//Close navbar when close button was clicked
 close.addEventListener("click", () => {
   nav.classList.remove("nav-open");
 });
+
+//Close navbar when overlay was cliked
 overlay.addEventListener("click", () => {
   nav.classList.remove("nav-open");
 });
+
+//Close navbar when menu was cliked
 menu_btn.addEventListener("click", () => {
   nav.classList.remove("nav-open");
 });
